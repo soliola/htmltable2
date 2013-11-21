2eed3ecdc683fc7f7d046590295bc20a92dbcd34
Developer User <developer@developer.local>
2013-11-21 22:30:34 +0100

    Push1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>phpinfo()</title>
+</head>
+<body>
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
+</body>
+</html>
